694da74ec25ffe9b423c50f3fde6bed43a17f404
dimfu <0arcetros@gmail.com>
2023-11-21 13:20:50 +0800

    add winston wrapper and exec middleware

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LeaderboardModule } from './leaderboard/leaderboard.module';
 import { ConfigModule } from '@nestjs/config';
 import { RaceChallengeModule } from './racechallenge/racechallenge.module';
 import leaderboardConfig from './configs/leaderboard.config';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+import { ExecutionTimeMiddleware } from './logger/logger.middleware';
 
 @Module({
   imports: [
@@ -13,10 +16,29 @@ import leaderboardConfig from './configs/leaderboard.config';
       isGlobal: true,
       load: [leaderboardConfig],
     }),
+    WinstonModule.forRoot({
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.colorize({ all: true }),
+            winston.format.timestamp({
+              format: 'YYYY-MM-DD HH:mm',
+            }),
+            winston.format.printf(({ level, message, timestamp }) => {
+              return `${timestamp} ${level}: ${message} `;
+            }),
+          ),
+        }),
+      ],
+    }),
     LeaderboardModule,
     RaceChallengeModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ExecutionTimeMiddleware).forRoutes('*');
+  }
+}
