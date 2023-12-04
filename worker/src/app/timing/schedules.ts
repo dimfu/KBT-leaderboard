6b9a3da3a869b2695802a737c782e6fec2cff63b
dimfu <0arcetros@gmail.com>
2023-12-04 22:12:36 +0800

    use 30 minutes schedule to prevent cfw maxing out

--- worker/src/app/timing/schedules.ts
+++ worker/src/app/timing/schedules.ts
@@ -3,27 +3,27 @@ import getTimingRecords from "./timing-records";
 
 export default async function scheduledTiming(event: ScheduledEvent, env: Env) {
   switch (event.cron) {
-    case '*/10 * * * *':
+    case '*/30 * * * *':
       await getTimingRecords(env, {
         include: { Gunma: ['pk_akina', 'ek_akagi', 'pk_usui_pass', 'ek_myogi'] }
       })
       break;
-    case '*/11 * * * *':
+    case '*/31 * * * *':
       await getTimingRecords(env, {
         include: { Ibaraki: ['ek_tsukuba_fruits_line', 'ek_tsukuba_fruits_line_snow'] }
       })
       break;
-    case '*/12 * * * *':
+    case '*/32 * * * *':
       await getTimingRecords(env, {
         include: { Kanagawa: ['ek_nanamagari', 'ek_tsubaki_line', 'hakoneTurnpike_wmmt5'] }
       })
       break;
-    case '*/13 * * * *':
+    case '*/33 * * * *':
       await getTimingRecords(env, {
         include: { Saitama: ['ek_sadamine', 'ek_tsuchisaka', 'rtp_maze_pass', 'shomaru'] }
       })
       break;
-    case '*/14 * * * *':
+    case '*/34 * * * *':
       await getTimingRecords(env, {
         include: {
           Shizuoka: ['ek_nagao'],
